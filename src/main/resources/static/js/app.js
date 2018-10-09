fd72c761b9483fa96159ff025874628705e6dc00
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2018-10-09 23:23:53 +0700

    aad datatables bootstrap

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,13 +1,51 @@
-var myApp = angular.module('myApp', ['ngResource']);
-
-myApp.config(function($resourceProvider){
-    $resourceProvider.defaults.stripTrailingSlashes = false;
-});
-
-myApp.controller('loginCtrl', function($scope){
-
-     console.log('AngularJS Kolaborasi Thymeleaf');
-
-     $scope.username = "";
-
+$(document).ready(function(){
+    var tabel = $('#courseTable')
+        .DataTable({
+            "sAjaxSource" : "/getcourses",
+            "sAjaxDataProp" : "",
+            "columns" : [
+                {
+                    "data": "courseid",
+                    "visible": false
+                },
+                {
+                    "data": "coursecode"
+                },
+                {
+                    "data": "name"
+                },
+                {
+                    "data": "createdBy"
+                },
+                {
+                    "data": "reviewaction",
+                    "width": "7%",
+                    "orderable": false,
+                    "searchable": false,
+                    "render": function(data, type, row, meta){
+                        const a='<a href="/coursestudents/'+row.courseid+'">Review</a>';
+                        return a;
+                    }
+                },
+                {
+                    "data": "reviews",
+                    "width": "9%",
+                    "searchable": false,
+                    "render": function(data, type, row, meta){
+                        const a='<a href="/questions/'+row.courseid+'">View Reviews</a>';;
+                        return a;
+                    }
+                },
+                {
+                    "data": "editaction",
+                    "orderable": false,
+                    "searchable": false,
+                    "width": "9%",
+                    "render": function(data, type, row, meta){
+                        const a='<a href="/editcourse/'+row.courseid+'">Edit Question</a>';
+                        return a;
+                    }
+                }
+            ]
+        })
 });
